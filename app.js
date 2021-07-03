d7d545a7b3d5b9dcb85c8ed9c014ced581631489
Aksiuszka <70382984+Aksiuszka@users.noreply.github.com>
2021-07-03 19:45:24 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const output = document.querySelector('.output');
+const input = document.querySelector('.input');
+
+const speechRecognition=window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new speechRecognition();
+
+recognition.onstart = function (){
+    console.log("Zacznij mówić");
+
+};
+//w evencie przetrzymuje to co mowimy
+recognition.onresult = function (event){
+    const currentText = event.resultIndex;
+    //dostep do tekstu
+    const transcript = event.results[currentText][0].transcript;
+    input.textContent = transcript;
+    outLoud(transcript);
+};
+
+
+
+output.addEventListener('click', ()=>{
+recognition.start();
+});
+
+function outLoud(message){
+    const utterance = new SpeechSynthesisUtterance();
+    utterance.text= message;
+    utterance.pitch = 1;  // a little lower
+    utterance.rate = 1;   // a little faster
+    utterance.volume = 1; // a little quieter
+    window.speechSynthesis.speak(utterance);
+};
